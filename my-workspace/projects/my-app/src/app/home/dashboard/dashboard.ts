beb530758a253884eae584521bee9fa6e26a7822
GRUPPOFIN\BERNARDI.I <ivan.bernardi@gruppofinservice.com>
2026-03-10 11:18:30 +0100

    merge dashboard

--- my-workspace/projects/my-app/src/app/home/dashboard/dashboard.ts
+++ my-workspace/projects/my-app/src/app/home/dashboard/dashboard.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProjectService } from '../../shared/service/project.service';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.html',
-  styleUrls: ['./dashboard.css']
+  styleUrls: ['./dashboard.css'],
+  imports:[TranslatePipe]
 })
-export class Dashboard implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class Dashboard {
+  protected readonly projectSvc = inject(ProjectService);
 }
