c3ce14e34d7f95bded73113f88f49b9ae3a2b0b6
GRUPPOFIN\Matteo.Vitale <Matteo.Vitale@Gruppofinservice.com>
2026-03-10 11:01:05 +0100

    Dashboard somma ore

--- my-workspace/projects/my-app/src/app/home/dashboard/dashboard.ts
+++ my-workspace/projects/my-app/src/app/home/dashboard/dashboard.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { UnionService } from '../../shared/service/union.service';
 
 @Component({
   selector: 'app-dashboard',
+  imports: [TranslatePipe],
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.css']
 })
-export class Dashboard implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class Dashboard {
+  protected readonly unionsSvc = inject(UnionService);
+  
+  protected totalHour(): number{
+    let sumHour = 0;
+    const union = this.unionsSvc.Union();
+    for(let i = 0; i < union.length; i ++){
+      sumHour += union[i].hours;
+    }
+    return sumHour;
   }
-
 }
