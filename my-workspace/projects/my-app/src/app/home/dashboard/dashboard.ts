8a3edaf768227a775be8456d3effd65f4340b778
GRUPPOFIN\Lorenzo.Bontempi <Lorenzo.Bontempi@Gruppofinservice.com>
2026-03-10 10:57:38 +0100

    dashboard parte user

--- my-workspace/projects/my-app/src/app/home/dashboard/dashboard.ts
+++ my-workspace/projects/my-app/src/app/home/dashboard/dashboard.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { UserService } from '../../shared/service/user.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.html',
-  styleUrls: ['./dashboard.css']
+  styleUrls: ['./dashboard.css'],
+  imports: [TranslatePipe]
 })
-export class Dashboard implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class Dashboard{
+  protected readonly UserSvc = inject(UserService);
 }
